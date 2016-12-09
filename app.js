1d67101f054ae9f6b715f7e91cb876000456251f
Yen Tran <tran.yentran@gmail.com>
2016-12-09 00:54:23 +0000

    Adds Mongoose model config

--- app.js
+++ app.js
@@ -2,12 +2,24 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose');
-    
+
+//APP CONFIG
 mongoose.connect('mongodb://localhost/blog_app');
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model('blog', blogSchema);
+
+//RESTFUL ROUTES
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('The BlogApp Server has started!');
 });
