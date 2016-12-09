8b0284e5ed4a9fae2d8019769cac2c6df99a1789
Yen Tran <tran.yentran@gmail.com>
2016-12-09 17:50:02 +0000

    adds show route

--- app.js
+++ app.js
@@ -51,6 +51,17 @@ app.post('/blogs', function(req, res){
    }); 
 });
 
+//SHOW ROUTE
+app.get('/blogs/:id', function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+       if (err) {
+           res.redirect('/');
+       } else {
+         res.render('show', {blog: foundBlog});  
+       }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log('The BlogApp Server has started!');
 });
